ba4d8c99c33de787345f034efb5d6a735361c883
Túlio Gabriel <tuliofindi@gmail.com>
2021-10-01 10:57:24 -0300

    Segunda atualização

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (!empty($_POST['email']) and !empty($_POST['password'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>ENTRAR</title>
 </head>
 
 <body>
